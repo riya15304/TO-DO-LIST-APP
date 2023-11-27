fe3acfe91b4a8788e08b5143bf276160e58ec56b
riya15304 <152092655+riya15304@users.noreply.github.com>
2023-11-26 22:22:00 -0800

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("t");
+const listContainer = document.getElementById("k");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let s = document.createElement("s");
+        s.innerHTML = "\u00d7";
+        li.appendChild(s);
+    }
+    inputBox.value = "";
+    saveData();
+}
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "S"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
